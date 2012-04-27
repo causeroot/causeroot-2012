88a168fabe5bda3794f3df91bf901a481e9775ec
Chris Barton <cbarton@inigral.com>
2012-04-26 22:05:55 -0700

    init 2

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails', '~> 2.0.2'
+gem 'bcrypt-ruby', '~> 3.0.1'
+
+gem 'thin', '~> 1.3.1'
+
+gem 'capistrano', '~> 2.12.0'
+
+gem 'debugger', '~> 1.1.1'
+
+gem 'devise', '~> 2.0.4'
+gem 'omniauth-facebook', '~> 1.2.0'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mensch::Application.config.secret_token = '2002a77e49b039f8a3ffd30bfde2702d797179e4ac221829836a818c24a1c1f4e996560eeef48d458c97d5fc35815e29b616298868b09190e78be8ea3811de58'
