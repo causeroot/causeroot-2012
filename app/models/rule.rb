65531397e4c899ed046bfcb4f52a2745938d0021
Gardner Bickford <gardner@invulnerable.org>
2012-06-24 18:58:01 -0700

    model changes for testing kaggle

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -2,29 +2,22 @@ class Rule < ActiveRecord::Base
   attr_accessible :name, :type, :xpath, :regex, :token, :modifier, :field_name
   has_many :rules
   
-  def getUrls(doc)
-  	# if xpath matches something then apply regex and return [value_after_regex]
-  	# else return []
-  	value = (doc/self.xpath)[0].innerHTML
-  	
-  end
-
   def get_urls(doc)
     # while xpath_with_token_replaced_by_modifier matches url
     # add url to returnValues[] array
+  	urls = Array.new
+  	(doc/self.xpath).each do |link|
+  		urls.push(link.attributes['href'])
+  	end
+  	return urls
   end
 
-  def name_values(doc)
-		nvs = Array.new
-
-  	# if xpath matches something
+  def extract_value(doc)
   	value = (doc/self.xpath)[0].innerHTML
   	
   	# then apply regex and return [:field, value_after_regex]
   	value = /self.regex/.match(value)
   	
-  	# then push field, value
-		nvs.push([self.field, value])
-		return nvs		
+		return value
   end  
 end
