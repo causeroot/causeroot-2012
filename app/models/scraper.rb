16168377c240cd1dacbb5341a3cf9bcffc2bff5f
Gardner Bickford <gardner@invulnerable.org>
2012-06-27 22:04:34 -0700

    fixed the fixtures

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -2,7 +2,7 @@ class Scraper < ActiveRecord::Base
   attr_accessible :name, :url_id, :site_id
   belongs_to :site
   has_one :rule
-  
+
   def scrape!(challenge)
   	#	foreach rule that returns name value pair, set the value in Challenge
 		# for each url returned by rules use factory to create child scraper that matches url
--- test/unit/scraper_test.rb
+++ test/unit/scraper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ScraperTest < ActiveSupport::TestCase
   test "scrape" do
-  	scraper = Scraper.findByName("Kaggle")
+  	scraper = Scraper.where(:name => "Kaggle")
   	
   	challenge = scraper.scrape!
   	assert_equal(challenge.title, "GigaOM WordPress Challenge, Powered by Splunk")
