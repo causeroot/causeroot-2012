e575ddc1dc3e490be16b5febd8b702f58d44495f
Gardner Bickford <gardner@invulnerable.org>
2012-07-25 11:17:06 -0700

    Added capistrano deploy script.

---
+++ config/deploy.rb
@@ -0,0 +1,38 @@
+require 'bundler/capistrano'
+
+set :user, 'deploy'
+set :domain, 'carrio.org'
+set :applicationdir, "appdir"
+
+set :application, "carrio"
+set :scm, :git
+set :repository,  "https://github.com/gardner/carrio.git"
+set :deploy_via, :checkout
+set :use_sudo, false
+set :scm_verbose, true
+set :deploy_to, "/home/deploy/carrio"
+
+
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  "127.0.0.1", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+set :rails_env, "production"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+# if you want to clean up old releases on each deploy uncomment this:
+after "deploy:restart", "deploy:cleanup"
