b1a0df206023edee35f2f9c0ac70750ac660a75c
Gardnerrails s <gbickfor@adobe.com>
2012-09-22 19:55:59 -0700

    passing rspec scaffolding tests

---
+++ spec/factories.rb
@@ -0,0 +1,13 @@
+# This will guess the User class
+FactoryGirl.define do
+  factory :challenge do
+    name "Challenge Name"
+    url  "http://challenge.com/url"
+    description Faker::Lorem.paragraphs(3)
+    award "$17,000"
+    deadline 1.years.from_now
+    post_date 1.year.ago
+    image_url "/assets/challenge.png"
+    rules Faker::Lorem.paragraphs(3)
+  end
+end
