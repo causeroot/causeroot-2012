df824bd193cf10c0397403bf51d22413b541b83b
Roux Heyns <rheyns@gmail.com>
2012-10-11 23:04:25 -0700

    make username accessible attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
   # attr_accessible :title, :body
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :username, :email, :password, :password_confirmation
 
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
