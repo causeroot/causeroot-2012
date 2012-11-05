abf35c38394a4da99a1b5b4b6cbb104b32cc8c88
gardner <gardner@invulnerable.org>
2012-11-04 18:54:39 -0800

    Added default keys to ssh_options

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :keep_releases, 1
 set :scm, :git
 set :user, 'ubuntu'
 set :use_sudo, false
-ssh_options[:keys] = '~/.ssh/openplaya.pem'
+ssh_options[:keys] = %w(~/.ssh/id_dsa ~/.ssh/id_rsa '~/.ssh/openplaya.pem')
 set :repository, "https://github.com/gardner/CauseRoot.git"
 set :deploy_to, "/home/ubuntu/#{application}"
 set :deploy_via, :remote_cache
