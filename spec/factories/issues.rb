9d3dbeb19a12a789a340cb4bfaa2dc2d04c3b347
gardner <gardner@invulnerable.org>
2012-12-01 18:18:55 -0800

    Updated factories to work with new models.

--- spec/factories/issues.rb
+++ spec/factories/issues.rb
@@ -1,9 +1,19 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  sequence(:iproblem) {|n| "#{Faker::Lorem.sentence}#{n}" }
+
   factory :issue do
-    problem "MyText"
-    cause_id 1
-    effect_id 1
+    problem { FactoryGirl.generate(:iproblem) }
+    factory :issue_with_causes do
+      ignore do
+        cause_count 5
+      end
+
+      after_create do |user, evaluator|
+        FactoryGirl.create_list(:cause, evaluator.cause_count, :user => user)
+      end
+    end
+
   end
 end
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,9 +1,11 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  sequence(:qname) {|n| "#{Faker::Lorem.sentence}#{n}" }
+  sequence(:qq) {|n| "#{Faker::Lorem.paragraph}#{n}" }
   factory :question do
-    question "Is MyString a problem?"
-    name "Stringage"
-    prob_count 1
+    question { FactoryGirl.generate(:qq) }
+    name { FactoryGirl.generate(:qname) }
+    problem_count 2
   end
 end
