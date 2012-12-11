d250524b51b6e16fce8f3efbd55e46df797fd601
Bustedblah <Luis.F.Carrio@gmail.com>
2012-12-10 21:54:14 -0800

    Finishing the game data extractor except for that quotes around the problem statements

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121129053134) do
+ActiveRecord::Schema.define(:version => 20121209010750) do
 
   create_table "causes", :force => true do |t|
     t.integer  "issue_id"
@@ -43,6 +43,11 @@ ActiveRecord::Schema.define(:version => 20121129053134) do
   end
 
   add_index "game_results_issues", ["game_result_id", "issue_id"], :name => "index_game_results_issues_on_game_result_id_and_issue_id", :unique => true
+
+  create_table "graphs", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "issues", :force => true do |t|
     t.text     "problem"
