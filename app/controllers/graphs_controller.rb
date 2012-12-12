0dc0a3e2238896489eb93bc1c12e379704fd1fa7
gardner <gardner@invulnerable.org>
2012-12-11 18:54:50 -0800

    fixed buglet in naming.

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -110,7 +110,7 @@ class GraphsController < ApplicationController
 
     csvstr = CSV.generate() do |csv|
 
-      csv << ["'Question_Themes'"] + question_set.map{|i| question_Themes[i-1] }
+      csv << ["'name'"] + question_set.map{|i| question_Themes[i-1] }
       idata.each do |k,v|    
           #csv << [problems[k-1]] + (question_set.map{|i| v[i].to_s })
           csv <<  ["'#{problems[k-1]}'"] + (question_set.map{|i| %Q['#{v[i]}'] })
