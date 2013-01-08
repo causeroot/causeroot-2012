e4669de84a2f82d19734603dc6e07968964b1fcf
gardner <gardner@invulnerable.org>
2013-01-08 01:06:33 -0800

    fixed the questions index test

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -24,7 +24,7 @@ describe QuestionsController do
   # Question. As you add validations to Question, be sure to
   # update the return value of this method accordingly.
   def valid_attributes
-    {}
+    FactoryGirl.attributes_for(:question)
   end
 
   # This should return the minimal set of values that should be in the session
@@ -36,9 +36,9 @@ describe QuestionsController do
 
   describe "GET index" do
     it "assigns all questions as @questions" do
-      question = Question.create! valid_attributes
+      question = Question.create valid_attributes
       get :index, {}, valid_session
-      assigns(:questions).should eq([question])
+      assigns(:questions).should include(question)
     end
   end
 
