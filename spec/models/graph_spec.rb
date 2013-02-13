38fa060ee8c1a854942dbb25dffbd9f9298d64a4
gardner <gardner@invulnerable.org>
2013-02-12 22:10:29 -0800

    added pending to unfinished test case

--- spec/models/graph_spec.rb
+++ spec/models/graph_spec.rb
@@ -5,5 +5,6 @@ describe Graph do
   it "should generate csv" do
     10.times { FactoryGirl.create(:game_result_with_issues) }
     str = Graph.grabdata
+    pending "Someone needs to write a test for graph"
   end
 end
