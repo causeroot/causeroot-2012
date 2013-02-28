bc6c0100068b731df45aa99ea4d28b746be57663
gardner <gardner@invulnerable.org>
2013-02-27 23:22:16 -0800

    enable user to reset password.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,14 @@
 class UserMailer < ActionMailer::Base
   default from: "from@example.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.reset_password_email.subject
+  #
+  def reset_password_email(user)
+    @user = user
+    @url  = "http://beta.causeroot.org/password_resets/#{user.reset_password_token}/edit"
+    mail(:to => user.email, :subject => "Your password has been reset")
+  end
 end
