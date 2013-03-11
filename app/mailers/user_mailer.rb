bf158774b95a6c9218c3407108595aca0ced9a4e
gardner <gbickfor@adobe.com>
2013-03-10 20:06:35 -0700

    password reset fixes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "info@causeroot.org"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
