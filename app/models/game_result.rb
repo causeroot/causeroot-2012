e1b02ea4afdd60c3ad58defbba5640d36ac0c66a
Bustedblah <Luis.F.Carrio@gmail.com>
2013-03-10 20:19:34 -0700

    Game Results controller now asks the right question - taks 100 msec

--- app/models/game_result.rb
+++ app/models/game_result.rb
@@ -12,25 +12,24 @@ class GameResult < ActiveRecord::Base
       self.same = false if (self.has_attribute? :same) && self.same.nil?
     end
 
+
   def self.pick_result(uId)
     @game_result = GameResult.new
-    question_randomizing_prob = 0.99
+    question_randomizing_prob = 0.70
 
     played_games = GameResult.select{|g| g.user_id == uId && g.skip == false}
 
-    if rand() > question_randomizing_prob && !played_games == nil
-    # TODO: does !played_games == nil do what I think it does?
-      played_games = GameResult.select{|g| g.user_id == 2 && g.skip == false}
+    if rand() < question_randomizing_prob && played_games != nil
       questions_asked = played_games.map{|g| g.question_id}
       question_list = Question.all.map{|q| q.id}
-
-      #next_question_id = question_list[question_list.map{|nv| questions_asked.count(nv)}.index(question_list.map{|nv| questions_asked.count(nv)}.min)]
-      #next_question_id = played_games.map{|g| g.question_id}.group_by { |e| e }.values.min_by(&:size).first
-
+      next_question_id = played_games.map{|g| g.question_id}.group_by { |e| e }.values.min_by(&:size).first
     else
       questions_asked = played_games.map{|g| g.question_id}.uniq
       next_question_id = questions_asked[rand(questions_asked.size)]
     end
+
+
+
 
     ## TODO: This needs to be modified at some point to not only ask a single question excessively/heavily when a new one is added to the mix
 
