10f734380c9aaab5c72d6a4e5f3ac4c488cb4f88
gardner <gbickfor@adobe.com>
2013-03-10 21:02:47 -0700

    added gmail support

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,15 @@ CauseRoot::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.smtp_settings = {
+	  :address              => "smtp.gmail.com",
+	  :port                 => 587,
+	  :domain               => 'causeroot.org',
+	  :user_name            => 'gardner@causeroot.org',
+	  :password             => 'pfjnoejqmezxlkiv',
+	  :authentication       => 'plain',
+	  :enable_starttls_auto => true  }
 
   # Enable threaded mode
   # config.threadsafe!
